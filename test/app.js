85ce447bf5eae383afdb1bb012369262982ea746
Bacra <bacra.woo@gmail.com>
2015-05-04 13:49:22 +0800

    Add: cache assert

--- test/app.js
+++ test/app.js
@@ -3,8 +3,16 @@ require('debug').enable('*');
 var expr = require('express')
 var app = expr();
 var port = 8022;
+var comboServ = require('../index.js');
 
-app.use(require('../index.js')());
+app.use('/static', comboServ({
+	root: __dirname+'/'+'static'
+}));
+
+app.use('/cache', comboServ({
+	root: __dirname+'/'+'static',
+	cachePath: __dirname+'/'+'cache',
+}));
 
 app.listen(8022);
 app.use(expr.static(__dirname));
